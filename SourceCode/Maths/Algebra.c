779a7db1246adf93f23723eba7c2c35810ba5eba
NavneetSaggu <navneetsaggu72@gmail.com>
2020-06-26 11:06:45 +0530

    Restructured, Added Files 		Moved files to Maths and Experiments directories 		Added new file Algebra.c

---
+++ SourceCode/Maths/Algebra.c
@@ -0,0 +1,7 @@
+int solveQuadraticEquation(int a, int b, int c) {
+	// return roots for quadtratic equation
+}
+
+int solveLinearEquation(int a1, int b1, innt c1, int a2, int b2, int c2) {
+	// solve simultaneous linear equation
+}
