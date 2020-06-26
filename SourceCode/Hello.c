ab6fe02a89f9df63856a9934ecde7cbaf7e2ac38
NavneetSaggu <navneetsaggu72@gmail.com>
2020-06-26 10:39:10 +0530

    Reverting Feature 		Inverse Patch for Useless Feature

--- SourceCode/Hello.c
+++ SourceCode/Hello.c
@@ -12,13 +12,6 @@ void RemoteChanges() {
 	printf("Remote Changes Are Done");
 }
 
-int UselessFunction() {
-	// KILL ME
-	// LIFE IS MEANINGLESS
-	// NOTHINGHAS ANY MEANING
-	return -1;
-}
-
 int main()
 {
 	Hello();
